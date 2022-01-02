828e300d4e1be6f720aefe8bbd0e37cd36d5ddc7
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2022-01-02 18:03:25 +0200

    Added all dependencies

---
+++ src/main/java/com/afriSystems/SchoolManagementSystems/SchoolManagementSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.afriSystems.SchoolManagementSystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afriSystems/SchoolManagementSystems/SchoolManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afriSystems.SchoolManagementSystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
